9fa0bd4ef8df56aea51dc39d9a89a9fbc645edf8
BubbaChe <1104504@abertay.ac.uk>
2017-04-25 11:52:07 +0100

    Json Display

---
+++ json_display.py
@@ -0,0 +1,42 @@
+import json
+import pandas as pd
+
+
+
+
+def display_json():
+    # pwned = pd.read_json('/caitlinroan@hotmail.com/pwned.json', orient='index')
+    # print pwned
+    print "hello"
+    with open("caitlinroan@hotmail.com/pwned.json") as pwned:
+        d = json.load(pwned)
+        pwncount = d.get('PwnCount', '')
+        site = d.get('Title', '')
+        date = d.get('BreachDate', '')
+        description = d.get('Description', '')
+        classes = d.get ('DataClasses', '')
+    df = pd.DataFrame([pwncount, site, date, description, classes]).T
+
+
+    # pwned_df = pd.DataFrame()
+    # count = 0;
+    # for l in open("caitlinroan@hotmail.com/pwned.json"):
+    #     try:
+    #         count += 1
+    #         if (count == 20001):
+    #             break
+    #         obj1 = json.loads(l)
+    #         df1 = pd.DataFrame(obj1, index=[0])
+    #         pwned_df = pwned_df.append(df1, ignore_index=True)
+    #     except ValueError:
+    #         line = line.replace('\\', '')
+    #         obj = json.loads(line)
+    #         df1 = pd.DataFrame(obj, index=[0])
+    #         pwned_df = pwned_df.append(df1, ignore_index=True)
+
+
+
+def main():
+    display_json()
+
+main()
